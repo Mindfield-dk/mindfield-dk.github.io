c4068cd1c3b5d2c487706652960670ab529c8b00
Git Actions <actions@github.com>
2020-09-09 13:24:02 +0000

    Github Actions published to master (build: 37)

---
+++ js/chunk-2d22d746.30c1b198.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("b-container",[t("b-row",[t("b-col",[t("h1",[n._v("Comming")])])],1)],1)},o=[],l=t("2877"),r={},u=Object(l["a"])(r,c,o,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.30c1b198.js.map
